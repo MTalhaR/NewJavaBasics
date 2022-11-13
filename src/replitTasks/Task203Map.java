f07d809d3fda745cc9051bcf2a2612db3fefb9bd
MTalhaR <talhariaz021@gmail.com>
2022-11-14 01:23:30 +0500

    class 29 & Quiz 07

--- src/replitTasks/Task203Map.java
+++ src/replitTasks/Task203Map.java
@@ -1,4 +1,16 @@
 package replitTasks;
-
+/*
+Create HashMap
+add values as below
+map.put("mango", 10);
+map.put("apple", 30);
+map.put("orange", 20);
+map.put("mango", 40);
+ map.put("mango", 40);
+Using iterator retrieve all keys and values if the format below:
+ */
 public class Task203Map {
+    public static void main(String[] args) {
+
+    }
 }

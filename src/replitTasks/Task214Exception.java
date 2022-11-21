52f7e27231ce088ebbe9ce742b7c726e1cdd700e
MTalhaR <talhariaz021@gmail.com>
2022-11-21 14:34:45 +0500

    repl exceptions

---
+++ src/replitTasks/Task214Exception.java
@@ -0,0 +1,33 @@
+package replitTasks;
+
+import java.util.Scanner;
+
+/* Create a custom Exception class
+In main class create a method that will check students grade
+if students grade is >90 then below exception should be thrown
+if students grade is below 90 print "You are a great student"
+In main method calls method gradeCheck and handle an Exception
+**Expected Output:**
+SyntaxStudentException: You are an exceptionally awesome student */
+public class Task214Exception {
+    public static void gradeCheck(int marks) throws SyntaxStudentException{
+        if (marks>90){
+            throw new SyntaxStudentException("You are an exceptionally awesome student");
+        } else {
+            System.out.println("You are a good student");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            gradeCheck(95);
+        } catch (SyntaxStudentException e){
+            System.out.println(e);
+        }
+    }
+}
+class SyntaxStudentException extends Exception {
+    SyntaxStudentException(String message){
+        super(message);
+    }
+}
